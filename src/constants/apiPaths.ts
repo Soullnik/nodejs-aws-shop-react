216270e6073ede9d4223ae06f10a265f50f558f2
Mikalai Lazitski <mlazitski@elinext.com>
2023-06-02 10:16:13 +0300

    change endpoint path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://m1a8n9eml2.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://m1a8n9eml2.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://m1a8n9eml2.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://m1a8n9eml2.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://m1a8n9eml2.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://efjj905kmg.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://efjj905kmg.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://efjj905kmg.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://efjj905kmg.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://efjj905kmg.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
