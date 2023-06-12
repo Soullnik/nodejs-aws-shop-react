07e563a4431f7078adef9ab8b7e58b47734783a6
Mikalai Lazitski <mlazitski@elinext.com>
2023-06-12 19:25:14 +0300

    fix: replace link to api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://1nl483hgx2.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://1nl483hgx2.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://1nl483hgx2.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://1nl483hgx2.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://1nl483hgx2.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://186295vebc.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://186295vebc.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://186295vebc.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://186295vebc.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://186295vebc.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
